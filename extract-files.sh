d01850d3ee98cede2b41e0a987f5d586df8343c1
Kyuofox <kyuofox@gmail.com>
2024-09-10 10:27:25 +0800

    andromeda: Allow gettid call for atfwd
    We never know what upstream is thinking:
    W ATFWD-daemon: libminijail[4756]: failed to get path of fd 3: No such file or directory
    W ATFWD-daemon: libminijail[4756]: allowing syscall: connect
    W ATFWD-daemon: libminijail[4756]: allowing syscall: fcntl
    W ATFWD-daemon: libminijail[4756]: allowing syscall: sendto
    W ATFWD-daemon: libminijail[4756]: allowing syscall: socket
    W ATFWD-daemon: libminijail[4756]: allowing syscall: writev
    W ATFWD-daemon: libminijail[4756]: logging seccomp filter failures
    E ATFWD-daemon: libminijail[4756]: blocked syscall: gettid
    
    Change-Id: I42e638f9f25395b37df57471fce68cc73e11e83f
    Signed-off-by: Kyuofox <kyuofox@gmail.com>

--- extract-files.sh
+++ extract-files.sh
@@ -58,6 +58,10 @@ function blob_fixup() {
         vendor/etc/media_codecs.xml|vendor/etc/media_codecs_vendor.xml)
             sed -Ei "/media_codecs_(google_audio|google_c2|google_telephony|vendor_audio)/d" "${2}"
             ;;
+        vendor/etc/seccomp_policy/atfwd@2.0.policy)
+            [ "$2" = "" ] && return 0
+            echo 'gettid: 1' >> ${2}
+            ;;
         vendor/lib64/libwvhidl.so)
             grep -q libcrypto_shim.so "${2}" || "${PATCHELF}" --add-needed "libcrypto_shim.so" "${2}"
             ;;
