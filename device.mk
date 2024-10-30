79524bc2f440f04cff45eb746fc582102727e005
Nicholas Magill <nmagill1979@gmail.com>
2024-10-30 17:53:35 +0000

    andromeda: Switch to QTI USB Gadget AIDL

--- device.mk
+++ device.mk
@@ -473,7 +473,7 @@ PRODUCT_SOONG_NAMESPACES += bootable/deprecated-ota
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.3-service.dual_role_usb \
-    android.hardware.usb.gadget@1.2-service-qti
+    android.hardware.usb.gadget-service.qti
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.usb.accessory.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.usb.accessory.xml \
