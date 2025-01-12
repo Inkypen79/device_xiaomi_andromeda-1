61d6b96bf3578968677c97a251cf3a3b5e0d56a5
Nicholas Magill <nmagill1979@gmail.com>
2025-01-12 21:46:28 +0000

    andromeda: Compile kernel with Clang 20.0.0

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,9 @@
 CLANGDIR=$PWD/prebuilts/clang/host/linux-x86
-CLANGVER=clang-r536225
+CLANGVER=clang-r547379
 if [ ! -d "${CLANGDIR}/${CLANGVER}" ]
 then
     GREEN='\033[0;32m'
     NC='\033[0m'
-    echo -e "${GREEN}Downloading clang 19.0.1${NC}"
+    echo -e "${GREEN}Downloading clang 20.0.0${NC}"
     git clone https://gitlab.com/inkypen/prebuilts_clang_host_linux-x86 -b ${CLANGVER} "${CLANGDIR}/${CLANGVER}" --single-branch
 fi
